3d86cbed724e3236d11f0e55dde39ade431195a1
Tobias Liebig <work@etobi.de>
2019-06-25 17:26:12 +0200

    TL-123 test

--- commentGithubPullrequest.php
+++ commentGithubPullrequest.php
@@ -1,3 +1,31 @@
 <?php
 
-var_dump($_ENV);
+if ($_ENV['TRAVIS_PULL_REQUEST'] != "false") {
+    $output = [];
+    $retval = '';
+		$command = 'curl'
+        . ' -s'
+        . ' -X GET'
+
+        . ' "'
+        . 'https://api.github.com/repos/'
+				. $_ENV['TRAVIS_PULL_REQUEST_SLUG']
+				. '/pulls/'
+        . ((int)$_ENV['TRAVIS_PULL_REQUEST'])
+        . '/commits'
+        . '"'
+        . ' -H "Authorization: token e45e67f37360af3f256892c91f64ab88067bdfac"';
+				echo $command;
+    exec(
+        $command,
+        $output,
+        $retval
+    );
+    $commitsInPullRequest = json_decode(implode('', $output), true);
+    foreach ($commitsInPullRequest as $commit) {
+    	$message = $commit['commit']['message'];
+			$matches = [];
+			preg_match('/[A-Za-z0-9]{2,4}-[0-9]+ /', $message, $matches);
+			var_dump($matches);
+    }
+}
